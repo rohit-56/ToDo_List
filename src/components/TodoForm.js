9d951d114e4bf44a26f36545c9ad19749766d6a8
Rohit <rohit.sharma2@hexaviewtech.com>
2022-05-18 16:09:32 +0530

    this component has input and button to add

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function TodoForm() {
+function TodoForm(props) {
+  const [input, setInput] = useState("");
+
+  const handleOnChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+    setInput("");
+  };
+
   return (
     <>
       <h1>ToDo List</h1>
-      <div className="todo-form">
-        <input type="text" placeholder="Add Todo Work" />
+      <form className="todo-form" onSubmit={handleOnSubmit}>
+        <input
+          type="text"
+          placeholder="Add Todo Work"
+          onChange={handleOnChange}
+          value={input}
+        />
         <button>Add ToDo</button>
-      </div>
+      </form>
     </>
   );
 }
